ca5d7a9f37701707bd022e3294cd98e59a809611
ileinirome <iromero@parkeasths.org>
2020-07-03 11:15:05 -0400

    First Commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
+from flask import render_template
+import string
+import random
+import pandas as pd 
+from datetime import datetime
 # from flask import request
 
 
@@ -13,4 +17,19 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html", time=datetime.now())
+
+@app.route('/game')
+def game():
+    df = pd.read_csv (r'list.csv')
+    print(df)
+    rows = df.size
+    print(df.iloc[15,0])
+
+    props = {
+        "letter":random.choice(string.ascii_letters), 
+        "category_1":df.iloc[random.randint(0,rows),0][3::],
+        "category_2":df.iloc[random.randint(0,rows),0][3::],
+        "category_3":df.iloc[random.randint(0,rows),0][3::]
+    }
+    return render_template("game.html", props=props, time=datetime.now())
